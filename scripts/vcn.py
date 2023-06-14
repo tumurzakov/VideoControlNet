d8dd8a05d6b754f4843653547cdae0088ab60590
Temir Umurzakov <temir@umurzakov.com>
2023-06-14 18:24:04 +0600

    Debug shapes

--- scripts/vcn.py
+++ scripts/vcn.py
@@ -274,6 +274,7 @@ class StableDiffusionProcessingImg2ImgVCN(StableDiffusionProcessingImg2Img):
       if self.vcn_fidelity_oriented_compensation:
           images = decode(samples)
           delta = fidelity_oriented_zeroshot_encoding(images)
+          print("\n===>", samples.shape, delta.shape, (samples - delta).shape)
           samples = encode(samples - delta)
 
       return samples
